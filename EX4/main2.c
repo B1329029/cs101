a9ac1e85bfd2b268c62b7c748576182f49aa5c8f
B1329029 <zh54923@gmail.com>
2024-12-12 15:02:49 +0800

    Update main2.c

--- EX4/main2.c
+++ EX4/main2.c
@@ -4,21 +4,22 @@ int main() {
     int flag = 1;
     long ipi = 0;
     int x = 0;
+    
     for (int i=3 ; i<=1000000 ; i+=2){
-        if (flag==0){
-            pi+=(4.0/i);
-            flag ++;
+        if (flag == 0){
+            pi +=(4.0/i);
+            flag++;
         }
-        else if (flag==1){
-            pi-=(4.0/i);
-            flag --;
+        else {
+            pi -= (4.0/i);
+            flag--;
         }
-        ipi = pi*1000000;
+        ipi = pi * 100000;
         if (ipi == 314159){
-            x=i;
+            x = i;
             break;
         }
     }
-    printf("%.5f",pi);
+    printf("%d %.5f",x,pi);
     return 0;
 }
